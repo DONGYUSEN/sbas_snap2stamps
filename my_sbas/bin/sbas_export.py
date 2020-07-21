f01bedcda8660a237bbef174add1545a75e88f12
DONGYUSEN <62499253+DONGYUSEN@users.noreply.github.com>
2020-07-21 11:12:43 +0800

    Update sbas_export.py

--- my_sbas/bin/sbas_export.py
+++ my_sbas/bin/sbas_export.py
@@ -3,6 +3,10 @@
 # Date: 21/06/2018
 # Version: 1.0
 
+# modify by dongyusen@gmail.com 2020.07.21
+
+
+
 # Step 1 : preparing slaves in folder structure
 # Step 2 : TOPSAR Splitting (Assembling) and Apply Orbit
 # Step 3 : Coregistration and Interferogram generation
@@ -52,6 +56,8 @@ finally:
 
 finishedfile=''
 finishedlist=[]
+file1=[]
+file2=[]
 
 
 
@@ -62,9 +68,31 @@ coregfolder=PROJECT+'/coreg'
 ifgfolder=PROJECT+'/ifg'
 head, tail = os.path.split(MASTER)
 print(tail)
-outputexportfolder=PROJECT+'/INSAR_'+tail[17:25]
 logfolder=PROJECT+'/logs'
+
+
+masterdate = tail[17:25]
+# print('masterdate: ', masterdate)
+
+outputexportfolder = PROJECT+'/INSAR_'+tail[17:25]
+outputexportfolder1 = outputexportfolder + '/SMALL_BASELINES'
 finishedfile=outputexportfolder+'/finished.txt'
+finished_insar=PROJECT+'/ifg/finished.txt'
+
+# print(finished_insar)
+
+print('Checking finished files in /ifg')
+with open(finished_insar, 'r') as f :
+    for line in f.readlines():
+        file1.append(line[0:8].strip())
+        file2.append(line[9:17].strip())
+maxfile_date = max(max(file1), max(file2))
+minfile_date = min(min(file1), min(file2))
+
+outputexportfolder2 = outputexportfolder1 + '/' + minfile_date + '_' + maxfile_date
+outgeo = outputexportfolder + '/geo'
+
+
 
 if not os.path.exists(outputexportfolder):
     os.makedirs(outputexportfolder)
@@ -72,6 +100,14 @@ if not os.path.exists(logfolder):
     os.makedirs(logfolder)
 if not os.path.exists(finishedfile):
     Path(finishedfile).touch()
+
+
+if not os.path.exists(outputexportfolder1):
+    os.makedirs(outputexportfolder1)
+if not os.path.exists(outputexportfolder2):
+    os.makedirs(outputexportfolder2)
+if not os.path.exists(outgeo):
+    os.makedirs(outgeo)
 
 
 outlog=logfolder+'/export_proc_stdout.log'
@@ -86,35 +122,39 @@ print(message)
 out_file.write(message)
 print(bar_message)
 out_file.write(bar_message)
+
+
 k=0
 for dimfile in glob.iglob(coregfolder + '/*.dim'):
     head, tail = os.path.split(os.path.join(coregfolder, dimfile))
     k=k+1
     message='['+str(k)+'] Exporting pair: master-slave pair '+tail+'\n'
     ifgdim = Path(ifgfolder+'/'+tail)
-    print(ifgdim)
+    # print(ifgdim)
+    if ifgdim.is_file():
+        print(message)
+        out_file.write(message)
+
     with open(finishedfile, 'r') as file :
         filedata = file.read()
     if dimfile in filedata:
         print(' Alreay processed, Check InSAR_XXXX/finished.txt ..............')
         continue
 
-    if ifgdim.is_file():
-        print(message)
-        out_file.write(message)
+
     with open(graphxml, 'r') as file :
         filedata = file.read()
 
         # Replace the target string
     filedata = filedata.replace('COREGFILE',dimfile)
     filedata = filedata.replace('IFGFILE', str(ifgdim))
-    filedata = filedata.replace('OUTPUTFOLDER',outputexportfolder)
+    filedata = filedata.replace('OUTPUTFOLDER',outputexportfolder2)
     # Write the file out again
     with open(graph2run, 'w') as file:
         file.write(filedata)
     # args = [ GPT, graph2run, '-c', CACHE, '-q', CPU]
     args = [ GPT, graph2run, '-q', CPU]
-    print(args)
+    # print(args)
 
     # Launching process
     process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
@@ -140,3 +180,44 @@ for dimfile in glob.iglob(coregfolder + '/*.dim'):
     out_file.write(bar_message)
 out_file.close()
 err_file.close()
+
+args = [ 'cp', outputexportfolder2 + '/dem/projected_dem.par', outputexportfolder +'/geo/' + masterdate +'_dem.par']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+args = [ 'cp', outputexportfolder2 + '/dem/projected_dem.rslc', outputexportfolder +'/geo/' + masterdate +'_dem.rslc']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+args = [ 'cp', outputexportfolder2 + '/geo/'+ masterdate + '.lat', outputexportfolder +'/geo/']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+args = [ 'cp', outputexportfolder2 + '/geo/'+ masterdate + '.lat.par', outputexportfolder +'/geo/']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+args = [ 'cp', outputexportfolder2 + '/geo/'+ masterdate + '.lon', outputexportfolder +'/geo/']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+args = [ 'cp', outputexportfolder2 + '/geo/'+ masterdate + '.lon.par', outputexportfolder +'/geo/']
+process = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+for line in iter(process.stdout.readline, b''):
+    print(line.rstrip())
+process.stdout.close()
+process.wait()
+print('-------------Finish exporting')
+print('-------------First, run mt_prep_snap ' + masterdate +  ' ' + outputexportfolder2  + ' 0.6 .......' + ' in ' + outputexportfolder)
+print('-------------Then run sb_parms_initial in matlab')
+print('               Good luck  :)                    ')
